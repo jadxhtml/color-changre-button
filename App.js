c87480b47413e9c5ed00984ad64b5bf524b901a5
jadxhtml <cuonggjadent@gmail.com>
2025-02-19 13:51:19 +0700

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,105 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Alert } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const colors = ['green', 'blue', 'brown', 'yellow', 'red', 'black'];
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: '',
+      backgroundColor: '#fff',
+    };
+  }
+
+  handleButtonClick = (newColor) => {
+    this.setState({ backgroundColor: newColor });
+  };
+
+  render() {
+    return (
+      <View style={[styles.container, { backgroundColor: this.state.backgroundColor }]}>
+        <ButtonComponent
+          backgroundColor="green"
+          onClick={this.handleButtonClick}
+          colorChange="green"
+        />
+        <ButtonComponent
+          backgroundColor="blue"
+          onClick={this.handleButtonClick}
+          colorChange="blue"
+        />
+        <ButtonComponent
+          backgroundColor="brown"
+          onClick={this.handleButtonClick}
+          colorChange="brown" 
+        />
+        <ButtonComponent
+          backgroundColor="yellow"
+          onClick={this.handleButtonClick}
+          colorChange="yellow" 
+        />
+        <ButtonComponent
+          backgroundColor="red"
+          onClick={this.handleButtonClick}
+          colorChange="red"
+        />
+        <ButtonComponent
+          backgroundColor="black"
+          onClick={this.handleButtonClick}
+          colorChange="black"
+        />
+      </View>
+    );
+  }
+}
+
+class TitleComponent extends Component {
+  render() {
+    const { title, color } = this.props;
+    return <Text style={[styles.title, { color: color }]}>{title}</Text>;
+  }
+}
+
+class ButtonComponent extends Component {
+  render() {
+    const { backgroundColor, message, onClick, colorChange } = this.props;
+    return (
+      <TouchableOpacity
+        style={[styles.button, { backgroundColor: backgroundColor }]}
+        onPress={() => onClick(colorChange)}
+      >
+        <Text style={styles.buttonText}>{backgroundColor}</Text>
+      </TouchableOpacity>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+  button: {
+    paddingVertical: 10,
+    paddingHorizontal: 70,
+    borderRadius: 5,
+    marginVertical: 10,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+  },
+  message: {
+    marginTop: 20,
+    fontSize: 18,
+    color: 'gray',
   },
 });
+
+export default App;
